d73bbbaae253a6e5928b6db1a31e67c3f05cfb75
Odd Martin Kveseth <odd.martin.kveseth@no.experis.com>
2023-03-09 22:20:11 +0100

    feat: Add CRUD methods for a Users class.

---
+++ src/main/java/com/experis_connect/models/dto/users/UsersPutDTO.java
@@ -0,0 +1,17 @@
+package com.experis_connect.models.dto.users;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsersPutDTO {
+    private int id;
+    private String created_at;
+    private String updated_at;
+    private String name;
+    private String picture;
+    private String status;
+    private String bio;
+    private String fun_fact;
+}
---
+++ src/main/java/com/experis_connect/services/users/UsersService.java
@@ -0,0 +1,7 @@
+package com.experis_connect.services.users;
+
+import com.experis_connect.models.Users;
+import com.experis_connect.services.CRUDService;
+
+public interface UsersService extends CRUDService<Users, Integer> {
+}
