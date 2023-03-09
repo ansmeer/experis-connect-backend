75c1863f715d6fdd4e03fc6d01dea44068a98c77
Ddayisme <122437344+Ddayisme@users.noreply.github.com>
2023-03-09 12:44:33 +0100

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO User (id_from_auth_provider, created_at, updated_at, name, picture, status, bio, fun_fact)
+VALUES (1, ("2022-01-01", "13:13:13"), ("2022-02-02", "14:14:14"), 'Eivind', 'random.jpg', 'single', 'From Oslo', 'Has a lazy eye');
+
+INSERT INTO User (id_from_auth_provider, created_at, updated_at, name, picture, status, bio, fun_fact)
+VALUES (2, ("2022-01-01", "13:13:13"), ("2022-02-02", "14:14:14"), 'Martin', 'random.jpg', 'single', 'From Oslo', 'Has a lazy eye');
+
+INSERT INTO User (id_from_auth_provider, created_at, updated_at, name, picture, status, bio, fun_fact)
+VALUES (3, ("2022-01-01", "13:13:13"), ("2022-02-02", "14:14:14"), 'William', 'random.jpg', 'single', 'From Oslo', 'Has a lazy eye');
