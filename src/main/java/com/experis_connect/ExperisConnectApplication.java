862dd5872b79154bf6aee48389dbc4ead761be95
Ddayisme <122437344+Ddayisme@users.noreply.github.com>
2023-03-09 09:35:09 +0100

    Initial commit

---
+++ src/main/java/com/experis_connect/ExperisConnectApplication.java
@@ -0,0 +1,13 @@
+package com.experis_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperisConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperisConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experis_connect/ExperisConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperisConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
