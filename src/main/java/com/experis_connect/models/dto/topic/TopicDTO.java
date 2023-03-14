b4d074e798e506b8a0342a4d256d0f4a0f2118aa
Luisa <luisa.mohle@no.experis.com>
2023-03-14 17:14:08 +0100

    fix: change topic property names to camelCase

--- src/main/java/com/experis_connect/models/dto/topic/TopicDTO.java
+++ src/main/java/com/experis_connect/models/dto/topic/TopicDTO.java
@@ -3,14 +3,12 @@ package com.experis_connect.models.dto.topic;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Getter
 @Setter
 public class TopicDTO {
     private int id;
-    private String created_at;
-    private String updated_at;
+    private String createdAt;
+    private String updatedAt;
     private String name;
     private String description;
     private String color;
