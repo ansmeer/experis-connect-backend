aec889aed9a196ed9ed5699b72e50ed7f88c6f0e
Odd Martin Kveseth <odd.martin.kveseth@no.experis.com>
2023-03-15 12:34:43 +0100

    feat: Make Topic endpoints for GET /topic with search/limit/offset params, make POST /topic add the user to the topic created, and make /topic/{id}/join for a user joining a topic.

--- src/main/java/com/experis_connect/services/topic/TopicService.java
+++ src/main/java/com/experis_connect/services/topic/TopicService.java
@@ -3,5 +3,9 @@ package com.experis_connect.services.topic;
 import com.experis_connect.models.Topic;
 import com.experis_connect.services.CRUDService;
 
+import java.util.Set;
+
 public interface TopicService extends CRUDService<Topic, Integer> {
+    Set<Topic> searchResultsWithLimitOffset(String name, int page, int size);
+    Topic addUserToTopic(String userId, int topicId);
 }
