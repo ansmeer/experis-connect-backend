beae10f13b8f29f0d573ac7d26faac25c0e5fb57
Odd Martin Kveseth <odd.martin.kveseth@no.experis.com>
2023-03-15 12:30:47 +0100

    fix: Was missing a decorator? How did the program run before that?

--- src/main/java/com/experis_connect/repositories/UsersRepository.java
+++ src/main/java/com/experis_connect/repositories/UsersRepository.java
@@ -2,6 +2,8 @@ package com.experis_connect.repositories;
 
 import com.experis_connect.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, String> {
 }
