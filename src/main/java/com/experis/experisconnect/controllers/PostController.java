5085261b5312b9546dbe4665d5078fadca6f3770
Luisa <luisa.mohle@no.experis.com>
2023-03-17 14:26:59 +0100

    refactor: replace custom parser with principal

--- src/main/java/com/experis/experisconnect/controllers/PostController.java
+++ src/main/java/com/experis/experisconnect/controllers/PostController.java
@@ -6,14 +6,13 @@ import com.experis.experisconnect.models.dto.post.PostDTO;
 import com.experis.experisconnect.models.dto.post.PostPostDTO;
 import com.experis.experisconnect.models.dto.post.PostPutDTO;
 import com.experis.experisconnect.services.post.PostService;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.security.Principal;
 import java.time.LocalDate;
-import java.util.Base64;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -66,52 +65,45 @@ public class PostController {
     }
 
     @GetMapping("/topic/{id}")
-    public ResponseEntity<Collection<PostDTO>> findAllPostsInATopic(@PathVariable int id, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset){
+    public ResponseEntity<Collection<PostDTO>> findAllPostsInATopic(@PathVariable int id, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset) {
         String searching = search.orElse("").toLowerCase();
         int limiting = limit.orElse(999999999);
         int offsetting = offset.orElse(0);
         return ResponseEntity.ok(postMapper.postToPostDTO(postService.findAllPostsInTopic(id, searching, limiting, offsetting)));
     }
+
     @GetMapping("/group/{id}")
-    public ResponseEntity<Collection<PostDTO>> findAllPostsInAGroup(@PathVariable int id, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset){
+    public ResponseEntity<Collection<PostDTO>> findAllPostsInAGroup(@PathVariable int id, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset) {
         String searching = search.orElse("").toLowerCase();
         int limiting = limit.orElse(999999999);
         int offsetting = offset.orElse(0);
         return ResponseEntity.ok(postMapper.postToPostDTO(postService.findAllPostsInGroup(id, searching, limiting, offsetting)));
     }
+
     @GetMapping("/user")
-    public ResponseEntity<Collection<PostDTO>> findAllPostsToAUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset){
-        String userId = getIdFromToken(token);
+    public ResponseEntity<Collection<PostDTO>> findAllPostsToAUser(Principal principal, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset) {
+        String userId = principal.getName();
         String searching = search.orElse("").toLowerCase();
         int limiting = limit.orElse(999999999);
         int offsetting = offset.orElse(0);
         return ResponseEntity.ok(postMapper.postToPostDTO(postService.findAllPostsToUser(userId, searching, limiting, offsetting)));
     }
+
     @GetMapping("/user/{senderId}")
-    public ResponseEntity<Collection<PostDTO>> findAllPostsToAUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @PathVariable String senderId, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset){
-        String userId = getIdFromToken(token);
+    public ResponseEntity<Collection<PostDTO>> findAllPostsToAUser(Principal principal, @PathVariable String senderId, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset) {
+        String userId = principal.getName();
         String searching = search.orElse("").toLowerCase();
         int limiting = limit.orElse(999999999);
         int offsetting = offset.orElse(0);
         return ResponseEntity.ok(postMapper.postToPostDTO(postService.findAllPostsToUserFromSpecificUser(userId, senderId, searching, limiting, offsetting)));
     }
+
     @GetMapping
-    public ResponseEntity<Collection<PostDTO>> findPostsUserIsSubscribedTo(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset){
-        String userId = getIdFromToken(token);
+    public ResponseEntity<Collection<PostDTO>> findPostsUserIsSubscribedTo(Principal principal, @RequestParam Optional<String> search, Optional<Integer> limit, Optional<Integer> offset) {
+        String userId = principal.getName();
         String searching = search.orElse("").toLowerCase();
         int limiting = limit.orElse(999999999);
         int offsetting = offset.orElse(0);
         return ResponseEntity.ok(postMapper.postToPostDTO(postService.findPostsUserSubscribedTo(userId, searching, limiting, offsetting)));
-    }
-
-    private String getIdFromToken(String token){
-        String[] chunks = token.split("\\.");
-        Base64.Decoder decoder = Base64.getUrlDecoder();
-        String payload = new String(decoder.decode(chunks[1]));
-        String[] payloadData = payload.split(",");
-        payloadData = payloadData[6].split(":");
-        String id = payloadData[1].replace("\"", "");
-
-        return id;
     }
 }
