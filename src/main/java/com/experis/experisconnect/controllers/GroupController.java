6a8ab7a689a3147b5084dc570149a57d6d4a5611
william-vil <vilhelmsennn@gmail.com>
2023-03-20 09:36:11 +0100

    feat: add apiresponses

--- src/main/java/com/experis/experisconnect/controllers/GroupController.java
+++ src/main/java/com/experis/experisconnect/controllers/GroupController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
